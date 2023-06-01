6b98287395824458284f4aecd9cd90aaf0944d6f
yincarrey <jean.agurton@utem.cl>
2023-06-01 06:15:40 -0400

    vector de 12 vectores y productos ordenados SKU

--- prueba.cpp
+++ prueba.cpp
@@ -7,80 +7,103 @@
 
 
 struct Producto {
-    double sku;
+    std::string sku;
     std::string nombre;
-    double monto;
-    double descuento;
-    std::string fecha;
+    std::string monto;
+    std::string descuento;
+    int fecha;
     std::string estado;
 };
+
+int obtenerMes(const std::string& fechaUTC) {
+    std::istringstream iss(fechaUTC); // Crear un flujo de entrada a partir de la cadena
+    std::string token;
+
+    // Leer los componentes de la fecha separados por '-'
+    std::getline(iss, token, '-'); // Ignorar el año
+    std::getline(iss, token, '-'); // Obtener el mes
+
+    int mes = std::stoi(token);
+
+    return mes;
+}
 
 bool cumpleCondicionEstado(const std::string& estado) {
     return estado == "AUTHORIZED" || estado == "FINALIZED";
 }
 
-std::vector<Producto> procesarArchivo(std::ifstream& file) {
-    std::vector<Producto> productos; // Vector de productos
+std::vector<std::vector<Producto>> procesarArchivo(std::ifstream& file) {
+    std::vector<std::vector<Producto>> productosPorMes(12); // Vector de 12 vectores para los meses
 
-    int productosLeidos = 0;
     if (!file) {
         std::cerr << "Error al abrir el archivo CSV" << std::endl;
-        return productos; // Devolver vector vacío en caso de error
+        return productosPorMes;
     }
 
     std::string line;
     std::getline(file, line); // Descartar la primera línea
 
-    while (std::getline(file, line) && productosLeidos < 200) {
+    long int limit = 0;
+
+    while (std::getline(file, line) && limit < 1000) {
         std::stringstream ss(line);
         std::string field;
 
-        Producto producto; // Crear una instancia de la estructura Producto
+        Producto producto;
 
         std::vector<std::string> campos = {"sku", "nombre", "monto", "descuento", "fecha", "estado"};
         std::vector<std::string> valores(6);
 
         for (int i = 0; i < campos.size(); i++) {
             std::getline(ss, field, ';');
-            // Eliminar las comillas dobles del campo
             field = field.substr(1, field.length() - 2);
             valores[i] = field;
         }
 
-        producto.sku = std::stod(valores[0]);
+        producto.sku = valores[0];
         producto.nombre = valores[1];
-        producto.monto = std::stod(valores[2]);
-        producto.descuento = std::stod(valores[3]);
-        producto.fecha = valores[4];
+        producto.monto = valores[2];
+        producto.descuento = valores[3];
+        producto.fecha = obtenerMes(valores[4]);
         producto.estado = valores[5];
 
-        // Verificar si cumple con la condición del estado
         if (cumpleCondicionEstado(producto.estado)) {
-            // Agregar el producto al vector solo si cumple la condición
-            productos.push_back(producto);
+            // Insertar el producto ordenadamente en el vector correspondiente al mes
+            auto& productosMes = productosPorMes[producto.fecha - 1];
+            auto insertPos = std::lower_bound(productosMes.begin(),
+                                              productosMes.end(),
+                                              producto,
+                                              [](const Producto& a, const Producto& b) {
+                                                  return a.sku < b.sku;
+                                              });
+            productosMes.insert(insertPos, producto);
         }
 
-        productosLeidos++;
+        limit++;
+
     }
 
     file.close();
 
-    return productos; // Devolver el vector de productos
+    return productosPorMes;
 }
 
 int main() {
     std::ifstream file("C:\\Users\\Jean\\Desktop\\Repositories\\c++\\archivo.csv");
-    std::vector<Producto> productos = procesarArchivo(file);
+    std::vector<std::vector<Producto>> productosPorMes = procesarArchivo(file);
 
-    // Mostrar el contenido del vector de productos
-    for (const Producto& producto : productos) {
-        std::cout << "sku: " << producto.sku << std::endl;
-        std::cout << "nombre: " << producto.nombre << std::endl;
-        std::cout << "monto: " << producto.monto << std::endl;
-        std::cout << "descuento: " << producto.descuento << std::endl;
-        std::cout << "fecha: " << producto.fecha << std::endl;
-        std::cout << "estado: " << producto.estado << std::endl;
-        std::cout << std::endl;
+    // Verificar si hay productos en el primer mes
+    if (!productosPorMes.empty() && !productosPorMes[0].empty()) {
+        std::cout << "Productos del primer mes:" << std::endl;
+
+        // Mostrar el contenido del primer mes
+        for (const Producto& producto : productosPorMes[0]) {
+            std::cout << "sku: " << producto.sku << std::endl;
+        }
+
+        std::cout << "Tamano del primer mes: " << productosPorMes[0].size() << " productos" << std::endl;
+    } else {
+        std::cout << "No hay productos en el primer mes." << std::endl;
     }
 
     return 0;
